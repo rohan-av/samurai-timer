c23f2aebc139a26c0cddd6d9cbdbc3bdcbfb4937
Rohan Arya Varma <rohanaryavarma@gmail.com>
2019-09-19 17:14:10 +0800

    Minor changes to title

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port);
 
